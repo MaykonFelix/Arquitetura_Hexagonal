c35530b384c5368a1975124035c92d969108f46d
root <root@Maykon>
2024-01-21 18:34:00 -0300

    :tada: commit initial

---
+++ src/app/util/TerminalUtil.ts
@@ -0,0 +1,9 @@
+import { terminal } from "terminal-kit";
+
+export default class TerminalUtil {
+  static titulo(text: string) {
+    terminal.clear();
+    terminal.magenta(`${text}\n`);
+    terminal.magenta(`-`.repeat(text.length) + `\n`);
+  }
+}
